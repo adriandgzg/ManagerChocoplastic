0db562077d004af733961c84c54bee4704341b73
Fran Alvarez <frank.alvalc@gmail.com>
2020-07-29 00:54:07 -0500

    tablas inventario y traspasos de producto

---
+++ app/Http/Controllers/ProductTransferController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductTransfer;
+use Illuminate\Http\Request;
+
+class ProductTransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductTransfer  $productTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductTransfer $productTransfer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductTransfer  $productTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductTransfer $productTransfer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductTransfer  $productTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductTransfer $productTransfer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductTransfer  $productTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductTransfer $productTransfer)
+    {
+        //
+    }
+}
---
+++ database/factories/ProductTransferFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProductTransfer;
+use Faker\Generator as Faker;
+
+$factory->define(ProductTransfer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
