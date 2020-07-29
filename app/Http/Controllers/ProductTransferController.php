5984dcac02606e2a17fde274a865cea7ef287e38
Fran Alvarez <frank.alvalc@gmail.com>
2020-07-29 03:28:38 -0500

    consulta de traspaso

--- app/Http/Controllers/ProductTransferController.php
+++ app/Http/Controllers/ProductTransferController.php
@@ -20,7 +20,47 @@ class ProductTransferController extends ApiResponseController
      */
     public function index()
     {
-        
+        try {
+            //Sucursal al que pertenece el Usuario App
+            //$vStore_PK = Auth::user()->store_id;
+
+
+            $vProducts = DB::table('product_transfers AS PT')
+                ->join('products AS P', 'P.prod_pk', '=', 'PT.prod_fk')
+                ->join('stores AS SO', 'SO.stor_pk', '=', 'PT.stor_fk_output')
+                ->join('stores AS SI', 'SI.stor_pk', '=', 'PT.stor_fk_input')
+                ->select(
+                    'PT.prtr_pk',
+                    'PT.prtr_quantity',
+                    'PT.prtr_observation',
+                    'PT.created_at',
+
+                    'P.prod_pk',
+                    'P.prod_identifier',
+                    'P.prod_name',
+                    'P.prod_description',
+                    'P.prod_image',
+                    'P.prod_bulk',
+
+                    'PT.stor_fk_output',
+                    'SO.stor_name AS stor_name_output',
+
+                    'PT.stor_fk_input',
+                    'SI.stor_name AS stor_name_input'
+                )
+                ->where('PT.prtr_status', '=', 1)
+                //->where('PF.stor_fk', '=', $vStore_PK)
+                ->orderByDesc('PT.prtr_pk')
+                ->get();
+            
+            return $this->dbResponse($vProducts, 200, null, 'Lista de Productos Frecuentes');
+          
+        } 
+        catch (Throwable $vTh) 
+        {
+            return $vTh;
+            return $this->dbResponse(null, 500, $vTh, 'Detalle Interno, informar al Administrador del Sistema.');
+        }
         
     }
 
@@ -67,7 +107,7 @@ class ProductTransferController extends ApiResponseController
             //Buscar el folio consecutivo
             $vSystem = System::select('syst_transfer')->first();
             $vsyst_transfer = $vSystem->syst_transfer;
-            $vprtr_identifier =  "Trans_" . $vsyst_transfer;
+            $vprtr_identifier =  "Tras_" . $vsyst_transfer;
 
 
             //Guardar Producto Traspaso
