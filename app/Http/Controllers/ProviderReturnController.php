039dc27f6f0bf03e551a65d8582319e1505941f7
Fran Alvarez <frank.alvalc@gmail.com>
2020-08-08 15:32:29 -0500

    validar la devolucion al proveedor

--- app/Http/Controllers/ProviderReturnController.php
+++ app/Http/Controllers/ProviderReturnController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use DB;
-use Validator;
 use Throwable;
+use Validator;
 use App\ProviderReturn;
-use Illuminate\Http\Request;
-use App\Http\Controllers\api\ApiResponseController;
+use App\ProductInventory;
 use App\ProviderPurchase;
+use Illuminate\Http\Request;
+use App\ProviderReturnDetail;
 use App\ProviderPurchaseDetail;
+use App\Http\Controllers\api\ApiResponseController;
+use PHPUnit\Framework\Constraint\IsFalse;
 
 class ProviderReturnController extends ApiResponseController
 {
@@ -388,21 +391,71 @@ class ProviderReturnController extends ApiResponseController
             $vprre_pk = $vInput['prre_pk'];
             $vremo_fk = $vInput['remo_fk'];
             $vprre_observation = $vInput['prre_observation'];   
+            $vVal_Dev = false;   
 
             $vProviderReturn = ProviderReturn::where('prre_pk', '=', $vprre_pk)->first();
 
             if($vProviderReturn)
             { 
-                //Modificar Devolución (Finalizar)
-                DB::table('provider_returns')
-                ->where('prre_pk', '=', $vprre_pk)
-                ->update([
-                    'prre_status' =>  2, 
-                    'remo_fk' =>  $vremo_fk, 
-                    'prre_observation' =>  $vprre_observation
-                ]);
 
-                return $this->dbResponse($vprre_pk, 200, null, 'Devolución Finalizada Correctamente');
+                //Validar Detallado de la Devolución
+                $vPRD = ProviderReturnDetail::where('prre_fk', '=', $vprre_pk)
+                        ->where('prrd_status', '=', 1)
+                        ->get();
+
+                foreach($vPRD as $vP)
+                {
+                    //Datos del Producto a devolver
+                    $vprod_fk = $vP->prod_fk;
+                    $vprrd_quantity = $vP->prrd_quantity;
+
+                    //Buscar el Producto en el Inventario de la Sucursal 
+                    $vPI = ProductInventory::where('prod_fk', '=', $vprod_fk)
+                            ->where('prin_status', '=', 1)
+                            ->where('stor_fk', '=', $vProviderReturn->stor_fk)
+                            ->first();
+
+                    if ($vPI) 
+                    {
+                        //Datos del producto en el Inventario
+                        $vprin_pk = $vPI->prin_pk; //Llave primaria del Inventario
+                        $vprin_stock = $vPI->prin_stock; //Stock actual
+
+                        //Validar la cantidad del producto
+                        if($vprin_stock > $vprrd_quantity)
+                        {
+                            //Si aplicar Devolucion
+                            $vVal_Dev = true;
+                        }
+                        else
+                        {
+                            //Producto Insuficiente para Devolución
+                            $vVal_Dev = false;
+
+                        }
+                        
+                    } 
+                    else 
+                    {
+                        //Producto NO Encontrado, NO se puede devolver
+                        $vVal_Dev = false;
+                    }
+                }
+
+                //Validar si es factible hacer toda la devolucion
+                if ($vVal_Dev) {
+                    //Modificar Devolución (Finalizar)
+                    DB::table('provider_returns')
+                    ->where('prre_pk', '=', $vprre_pk)
+                    ->update([
+                        'prre_status' =>  2, 
+                        'remo_fk' =>  $vremo_fk, 
+                        'prre_observation' =>  $vprre_observation
+                    ]);
+                    return $this->dbResponse($vprre_pk, 200, null, 'Devolución Finalizada Correctamente');
+                } else {
+                    return $this->dbResponse($vprre_pk, 501, null, 'Artículos insuficientes para devolución. Revisar Inventario Actual');
+                }
             }
             else
             {
