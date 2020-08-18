c3610f76733c350b907adc0032f1faada08fe9ca
Fran Alvarez <frank.alvalc@gmail.com>
2020-08-17 20:19:13 -0500

    publicacion a produccion

--- database/seeds/ProductCategorySeeder.php
+++ database/seeds/ProductCategorySeeder.php
@@ -18,55 +18,68 @@ class ProductCategorySeeder extends Seeder
 
         ProductCategory::create([
             'prca_pk' => 1,
-            'prca_name' => "AB"
+            'prca_name' => "AB",
+            'prca_abbreviation' => "ABARROTES"
         ]);
         ProductCategory::create([
             'prca_pk' => 2,
-            'prca_name' => "AC"
+            'prca_name' => "AC",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 3,
-            'prca_name' => "ACT"
+            'prca_name' => "ACT",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 4,
-            'prca_name' => "ALM"
+            'prca_name' => "ALM",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 5,
-            'prca_name' => "ARZ"
+            'prca_name' => "ARZ",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 6,
-            'prca_name' => "BAL"
+            'prca_name' => "BAL",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 7,
-            'prca_name' => "BOL"
+            'prca_name' => "BOL",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 8,
-            'prca_name' => "BYF"
+            'prca_name' => "BYF",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 9,
-            'prca_name' => "CAF"
+            'prca_name' => "CAF",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 10,
-            'prca_name' => "CHI"
+            'prca_name' => "CHI",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 11,
-            'prca_name' => "DEB"
+            'prca_name' => "DEB",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 12,
-            'prca_name' => "DED"
+            'prca_name' => "DED",
+            'prca_abbreviation' => "AB"
         ]);
         ProductCategory::create([
             'prca_pk' => 13,
-            'prca_name' => "DEI"
+            'prca_name' => "DEI",
+            'prca_abbreviation' => "AB"
         ]);
     }
 }
