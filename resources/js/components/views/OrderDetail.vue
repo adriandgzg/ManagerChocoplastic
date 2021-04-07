2dbe69968e87457f7371b5926f6b26d662e2bc49
Fran Alvarez <frank.alvalc@gmail.com>
2021-04-06 22:36:54 -0500

    calculo de iva e ieps de orden de compra

--- resources/js/components/views/OrderDetail.vue
+++ resources/js/components/views/OrderDetail.vue
@@ -543,11 +543,10 @@ export default {
                 if (this.desserts[i].prod_iva == 1)
                     this.iva = this.iva + ((importeDescuento / (1 + (this.desserts[i].syst_iva / 100))) * (this.desserts[i].syst_iva / 100));
                 if (this.desserts[i].prod_ieps == 1)
-                    this.ieps = this.ieps + ((importeDescuento) * (this.desserts[i].syst_ieps / 100));
-
+                    this.ieps = this.ieps + ((importeDescuento / (1 + (this.desserts[i].syst_ieps / 100))) * (this.desserts[i].syst_ieps / 100));
             }
 
-            this.total = this.subtotal + this.ieps;
+            this.total = this.subtotal ;
         },
 
         finalizar() {
