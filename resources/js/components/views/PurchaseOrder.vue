2795a2420045bea8ebccfe46fb590fb51e4db930
Fran Alvarez <frank.alvalc@gmail.com>
2021-04-06 22:29:18 -0500

    orden de compra calculo correcto ieps

--- resources/js/components/views/PurchaseOrder.vue
+++ resources/js/components/views/PurchaseOrder.vue
@@ -622,10 +622,9 @@ export default {
 
                 this.subtotal = this.subtotal + (importeDescuento);
                 if (this.desserts[i].prod_iva == 1)
-                    this.iva = this.iva + ((importeDescuento  / (1 + (this.desserts[i].syst_iva / 100))) * (this.desserts[i].syst_iva / 100));
+                    this.iva = this.iva + ((importeDescuento / (1 + (this.desserts[i].syst_iva / 100))) * (this.desserts[i].syst_iva / 100));
                 if (this.desserts[i].prod_ieps == 1)
-                    this.ieps = this.ieps + ((importeDescuento) * (this.desserts[i].syst_ieps / 100));
-
+                    this.ieps = this.ieps + ((importeDescuento / (1 + (this.desserts[i].syst_ieps / 100))) * (this.desserts[i].syst_ieps / 100));
             }
 
             this.total = this.subtotal;
