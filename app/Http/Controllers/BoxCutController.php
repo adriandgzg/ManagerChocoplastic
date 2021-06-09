4d7245ddaa84a165cc4cbdb50ee67d4e9a9e9cf5
Fran Alvarez <frank.alvalc@gmail.com>
2021-06-09 13:54:57 -0500

    cambio en reporte de corte de caja

--- app/Http/Controllers/BoxCutController.php
+++ app/Http/Controllers/BoxCutController.php
@@ -389,13 +389,22 @@ class BoxCutController extends ApiResponseController
 
                     'BC.bocu_initialamount', //Saldo Inicial
                     DB::raw("
-                        $vCobrosCredit + 
-                            (
-                                SELECT SUM(CPA.cpam_amount) AS totalcharge 
-                                FROM client_payment_amounts AS CPA 
-                                INNER JOIN client_sales AS CS ON CS.clsa_pk = CPA.clsa_fk
-                                WHERE CPA.cpam_status = 1 AND CPA.bocu_fk = BC.bocu_pk AND CPA.pash_fk = 1 AND CS.pame_fk = 1
-                            ) AS totalcharge
+                        $vCobrosCredit 
+                        + 
+                        (
+                            SELECT SUM(CPA.cpam_amount) AS totalcharge 
+                            FROM client_payment_amounts AS CPA 
+                            INNER JOIN client_sales AS CS ON CS.clsa_pk = CPA.clsa_fk
+                            WHERE CPA.cpam_status = 1 AND CPA.bocu_fk = BC.bocu_pk AND CPA.pash_fk = 1 AND CS.pame_fk = 1
+                        ) 
+                        +
+                        (
+                            SELECT SUM(CPA.cpam_amount) AS totalcambio
+                            FROM client_payment_amounts AS CPA 
+                            INNER JOIN client_sales AS CS ON CS.clsa_pk = CPA.clsa_fk
+                            WHERE CPA.cpam_status = 1 AND CPA.bocu_fk = BC.bocu_pk AND CPA.pash_fk = 6 AND CS.pame_fk = 1
+                        )
+                        AS totalcharge
                     "), //Total Cobros
                     DB::raw("
                         (
