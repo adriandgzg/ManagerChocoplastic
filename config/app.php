a2644241bf5c6fcbfe45c459ab0fe2dc50aef8ae
Braulio OC <braulioolan@gsinnovation.com.mx>
2021-08-10 17:48:24 -0500

    Change default app in providers purchases

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'parent store' => env('PARENT_STORE_ID', 1),
 
     /*
     |--------------------------------------------------------------------------
